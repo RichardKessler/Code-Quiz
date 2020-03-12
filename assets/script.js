63f981a74b22625a709280199347be279b05f21a
JidMenosta <jidmenosta@gmail.com>
2020-03-12 18:08:06 -0400

    added a container for the quiz with buttons for answers and wrote JS for later access to those elements

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+//Declare variables to be accessed
+//need the functions for the timer and game start button to activate
+
+const timeEl = document.querySelector("#timer");
+const quizEl = document.querySelector("#quiz-container");
+const btn1El = document.querySelector("#btn1");
+const btn2El = document.querySelector("#btn2");
+const btn3El = document.querySelector("#btn3");
+const btn4El = document.querySelector("#btn4");
+
+
+const lostTime = 10;
+
+
+var highScore = 0;
+var playerScore = 0;
+
+
+
+
+
+
+
+
+
+
+
+// Play button on click run the game logic
