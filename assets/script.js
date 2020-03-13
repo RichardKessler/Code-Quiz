ea73eddf3dc1df98102cf6c05fca8f81cb05686f
JidMenosta <jidmenosta@gmail.com>
2020-03-13 15:07:29 -0400

    added my question and answer array

--- assets/script.js
+++ assets/script.js
@@ -19,19 +19,62 @@ var playerScore = 0;
 
 
 
-const questArr1 = ["Question 1 ", ];
-const questArr2 = ["Question 2 ", ];
-const questArr3 = ["Question 3 ", ];
-const questArr4 = ["Question 4 ", ];
-const questArr5 = ["Question 5 ", ];
+const questArr = [{
+        question: 'Which of these is not a javascript data type?',
+        answers: [{
+                text: 'Sting',
+                correct: false
+            },
+            { text: 'Boolean', correct: false },
+            { text: 'HTML', correct: true },
+            { text: 'Number', correct: false }
+        ]
+    },
+    {
+        question: 'Which of these is not a looping structure?',
+        answers: [{ text: 'For', correct: false },
+            { text: 'While', correct: false },
+            { text: 'Do-while', correct: false },
+            { text: 'Loop', correct: true }
+        ]
+    },
+    {
+        question: 'Which of these is not a type of pop-up box?',
+        answers: [{ text: 'Card', correct: true },
+            { text: 'Prompt', correct: false },
+            { text: 'Alert', correct: false },
+            { text: 'Confirm', correct: false }
+        ]
+    },
+    {
+        question: 'Which of these is not a boolean operator in Javascript?',
+        answers: [{ text: '&&', correct: false },
+            { text: '%%', correct: true },
+            { text: '||', correct: false },
+            { text: '!', correct: false }
+        ]
+    },
+    {
+        question: 'Which of these does not declare a variable?',
+        answers: [{ text: 'Var', correct: false },
+            { text: 'Let', correct: false },
+            { text: 'Const', correct: false },
+            { text: 'Add', correct: true }
+        ]
+    }
+]
 
 
 
+
+function gameTime(function, 1000) {
+    var gt =
+}
 
 
 
 
 // Play button on click run the game logic
 $("#start").click(function() {
-    timeEl.textContent = "Timer Left: " +
+    timeEl.textContent = "Time Left: " + gameTime();
 })
